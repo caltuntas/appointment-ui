04e0ac7056ec2bee0a066dbc046b7edc07d2afe9
cihataltuntas <btabcdef>
2020-05-03 17:55:58 +0300

    chore: appointment list component is added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    children: [
+      {
+        path: 'appointments',
+        component: AppointmentListComponent
+      }
+    ],
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HeaderComponent } from './header/header.component';
 import { MaterialModules } from './material.module';
 import { HomeComponent } from './home/home.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SidenavComponent
+    SidenavComponent,
+    AppointmentListComponent
   ],
   imports: [
     BrowserModule,
